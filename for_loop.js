634ec102f38ddb05a2f9585a11dd79528d0e1cb9
Sourav Sarker <souravsarker036@gmail.com>
2025-08-02 23:54:07 +0600

    for of loop

---
+++ for_loop.js
@@ -0,0 +1,12 @@
+// const numbers = [12, 98, 35, 36, 37];
+// console.log(numbers)
+
+// for(num of numbers){
+//     console.log(num)
+// }
+const fruits = ['apple','orange','mango']
+
+for(const fruit of fruits){
+    console.log(fruit);
+    console.log('I want to eat');
+}

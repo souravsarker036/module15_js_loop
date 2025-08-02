a77984462b25f328e6be0316686a3b24b9cff70e
Sourav Sarker <souravsarker036@gmail.com>
2025-08-03 00:32:39 +0600

    while loop

---
+++ sum.js
@@ -0,0 +1,19 @@
+// let num = 1;
+// let sum = 0;
+// while(num <=10){
+//     console.log(num);
+//     sum = sum +num;
+//     console.log('sum:', sum);
+//     num++
+// }
+
+// ---even number-----
+
+let num = 1;
+while(num <=10){
+    console.log(num);
+    if(num % 2 === 0){
+        console.log('even number', num)
+    }
+    num++;
+}
